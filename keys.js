6154ce26b90db909a6622ee553448170c55a358c
psharif <psharif@asu.edu>
2018-02-12 17:13:42 -0800

    Added initial request for Twitter Info

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'wNbbEIRJtVBOiYQMyUhEuh7Ky',
+  consumer_secret: 'ce9e5xW0PsR7DoECFX15GK09bD9ANDTWySeDygktMAcYDCY7x4',
+  access_token_key: '962426537017466880-wANmooJRIXrz0vJ9Ei7oaJuhtQZoNeD',
+  access_token_secret: 'narz1JJcf37GM4qR7x5A7s11q9e9fZVJA3bYBOLofPp0V',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,55 +1,17 @@
+//var twitterKeys = require("keys.js");
+var twitterKeys = require("./keys.js");
+var Twitter = require("twitter");
 var request = require("request");
+var twitterKeys = require("./keys.js");
 
+var twitterClient = new Twitter(twitterKeys);
 
-// INSTRUCTIONS:
-// ---------------------------------------------------------------------------------------------------------
-// Level 1:
-// Take any movie with a word title (ex: Cinderella) as a Node argument and retrieve the year it was created
-
-// Level 2 (More Challenging):
-// Take a move with multiple words (ex: Forrest Gump) as a Node argument and retrieve the year it was created.
-// ---------------------------------------------------------------------------------------------------------
-
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-// ...
-var request = require("request");
-
-var movieName ="";
-
-console.log(process.argv);
-
-// Grab or assemble the movie name and store it in a variable called "movieName"
-
-movieName += process.argv[2];
-
-
-console.log(movieName);
-
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-
-
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
-
-
-// Then create a request to the queryUrl
-// ...
-request(queryUrl, function(error, response, body){
-
-  console.log(body);
-
-  // If the request is successful
-  // ...
-  if(!error && response.statusCode === 200){
-
-  // Then log the Release Year for the movie
-  // ...
-  	console.log(JSON.parse(body).Released);
-
+var params = {screen_name: 'AliasPsharif',
+			  count: "3"};
+twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    tweets.forEach(function(el){
+    	console.log(el.text);
+    });
   }
-
-
 });
-
-
